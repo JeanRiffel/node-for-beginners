c85e9c90b784859ece3931e3d2cc5932451dd89d
jeanriffel <jeanriffel1@gmail.com>
2022-03-23 13:45:11 -0300

    adding files

---
+++ app-21-03-2022/index.js
@@ -0,0 +1,22 @@
+const name = 'jean';
+let   name2 = 'jean';
+
+const idade = 60;
+const peso = 155.60; 
+
+const ehAzul = true;
+
+const primeiraFuncao = (x, funcao)=>{
+    console.log('primeira funcao');
+    funcao(x);    
+}
+
+const segundaFuncao = (parametro)=>{
+    console.log(parametro);
+}
+
+const main = ()=>{
+    console.log('Hello world');
+}
+
+main();
