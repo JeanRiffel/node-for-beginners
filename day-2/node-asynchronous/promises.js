c0d8f6cc7e2aacc7fd9f294c80cf0269961beaf5
jeanriffel <jeanriffel1@gmail.com>
2022-09-09 23:16:25 -0300

    refactor: promises-file

--- day-2/node-asynchronous/promises.js
+++ day-2/node-asynchronous/promises.js
@@ -1,45 +1,50 @@
-const obterIDUsuario = ()=>{
-    console.log('ID Usuário', 100);    
+const getUserId = ()=>{
+    console.log('User Id', 100);    
     return 100;    
 }
 
-const consultarDadosUsuario = (idUsuario )=>{
+const getDataUser = (userId)=>{
     return new Promise ( (resolve, reject )=>{
         try{
             setTimeout( ()=>{ 
-                console.log('consultando dados usuário');
-                const dados = {
-                    idUsuario,
-                    nome : 'john',
-                    idade : 30}
-                resolve(dados);
-            }, 6000);            
+                console.log('Querying user data');
+                const data = {
+                    userId,
+                    name: 'john',
+                    age: 30}
+                resolve(data);
+            }, 3000);            
         }catch(error){
-            reject(error)
+            reject(error);
         }
     });
 };
 
-const ehMaiorIdade = (dadosUsuario )=>{
-    
-    if (!dadosUsuario){ console.log('não tem dados para idade'); return }
-    if (!dadosUsuario.idUsuario){ console.log('não encontrado idUsuario no objeto'); return }
+const isOfAge = (userData)=>{    
+    if (!userData){
+        console.log('Age data not found');
+        return false;
+    }
+    if (!userData.userId){
+        console.log('User Id not found');
+        return false;
+    }
 
-    if (dadosUsuario.idade >= 18){
-        console.log('eh maior de idade ');
+    if (userData.age >= 18){
+        console.log('Is Of Age');
         return true ;
     }else
-    if (dadosUsuario.idade < 18){
-        console.log('não eh maior de idade');  
+    if (userData.age < 18){
+        console.log('Is NOT Of Age');  
         return false;  
     }
 }
 
 const main = ()=>{
-    const idUsuario = obterIDUsuario();    
-    consultarDadosUsuario(idUsuario)
-        .then( dadosUsuario =>{
-            ehMaiorIdade(dadosUsuario);
+    const userId = getUserId();    
+    getDataUser(userId)
+        .then((dataUser) =>{
+            isOfAge(dataUser);
         })
         .catch( (error)=>{
             console.error(error);
